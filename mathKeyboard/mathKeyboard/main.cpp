ded06f13a6cf8e0d184d59b7ef93c29d4655e938
teterevlev <o.damage@gmail.com>
2017-02-15 17:24:34 +0300

    Revert "Revert "Added firmware""
    This reverts commit 6ca42c9610a6582ae2af47852e6e146049f42227.

---
+++ mathKeyboard/mathKeyboard/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * mathKeyboard.cpp
+ *
+ * Created: 15.02.2017 13:04:36
+ * Author : odama
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
